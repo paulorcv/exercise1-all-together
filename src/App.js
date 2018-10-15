ff729188c634e736a3ae34810caa8cc0e5c53025
Paulo D Vasconcelos <paulorcv@MacBook-Pro-de-Paulo.local>
2018-10-15 20:08:13 -0300

    Criacao da tela sem componentizacao

--- src/App.js
+++ src/App.js
@@ -10,6 +10,35 @@ React and prepare you for your first project.
 The instructions for this project are located in the `instructions.md` file.
 */
 
+const users = [
+  {
+    username: 'paulorcv',
+    firstName: 'Paulo',
+    lastName: 'Campos',
+    numberOfGames: 1
+  },
+  {
+    username: 'tatuzim',
+    firstName: 'Tatu',
+    lastName: 'Bolinha',
+    numberOfGames: 1
+  },
+  {
+    username: 'tonim',
+    firstName: 'Antonio',
+    lastName: 'Carlos',
+    numberOfGames: 2
+  },
+
+];
+
+const user = {
+  userName: 'amorinha',
+  firstName: 'Amora',
+  lastName: 'Maria',
+  numberOfGames: 0
+}
+
 class App extends Component {
   render() {
     return (
@@ -18,6 +47,37 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">ReactND - Coding Practice</h1>
         </header>
+        <div className='FormUser'>
+          <p>
+          <input type='text'
+                 placeholder='Enter username...'
+                 value={user.userName} />
+          </p>
+          <p>
+          <input type='text'
+                 placeholder='Enter First Name...'
+                 value={user.firstName} />
+          </p>      
+          <p>
+          <input type='text'
+                 placeholder='Enter Last Name...'
+                 value={user.lastName} />
+          </p>              
+          <p>
+            <button>ADD</button>
+          </p>
+        </div>
+
+        <div className='UsersGamesList'>
+          <h2>Users</h2>
+          <ol>
+            {users.map(user=>(
+              <li><b>{user.username}</b> has played 0 games</li>
+            ))}
+            
+          </ol>  
+        </div>
+
       </div>
     );
   }
